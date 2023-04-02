5b45f70a9cb8b8a207b4d4dbc66df33b46e8cc18
mambbi <88196706+mambbi@users.noreply.github.com>
2023-04-03 01:36:56 +0700

    Update ManageHospital.java

--- HW_Lecture6/src/ManageHospital.java
+++ HW_Lecture6/src/ManageHospital.java
@@ -6,7 +6,7 @@ public class ManageHospital {
     public ManageHospital(int id, String name, int age) {
         this.id = id;
         this.name = name;
-        this.age = age;
+        this.age =age;
     }
 
     // kiem tra tuoi:
